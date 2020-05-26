2095c1e2e3610b4b4f2f4cb238549d095c77d039
NehaChopra <nchopra456@gmail.com>
2020-05-26 18:29:43 +0530

    Day 3

---
+++ src/heapSort/SortColors.java
@@ -0,0 +1,50 @@
+package heapSort;
+
+// https://leetcode.com/problems/sort-colors/
+
+public class SortColors {
+	public static void sortColors(int[] inputArray) {
+		for (int index = inputArray.length / 2 - 1; index >= 0; index--) {
+			heapify(index, inputArray.length, inputArray);
+		}
+
+		for (int index = inputArray.length - 1; index >= 0; index--) {
+			swap(0, index, inputArray);
+			heapify(0, index, inputArray);
+		}
+	}
+
+	public static void heapify(int index, int end, int[] inputArray) {
+		int largest = index;
+		int left = leftChild(index);
+		int right = rightChild(index);
+		if (left < end && inputArray[left] > inputArray[largest]) {
+			largest = left;
+		}
+		if (right < end && inputArray[right] > inputArray[largest]) {
+			largest = right;
+		}
+		if (largest != index) {
+			swap(index, largest, inputArray);
+			heapify(largest, end, inputArray);
+		}
+	}
+
+	public static int leftChild(int index) {
+		return ((2 * index) + 1);
+	}
+
+	public static int rightChild(int index) {
+		return ((2 * index) + 2);
+	}
+
+	public static void swap(int iIndex, int jIndex, int[] inputArray) {
+		int temp = inputArray[iIndex];
+		inputArray[iIndex] = inputArray[jIndex];
+		inputArray[jIndex] = temp;
+	}
+
+	public static void main(String[] args) {
+		sortColors(new int[] { 2, 0, 2, 1, 1, 0 });
+	}
+}
