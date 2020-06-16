2096975bd95dabc367582c22de3d2748b4eb3ba1
NehaChopra <nchopra456@gmail.com>
2020-06-16 21:39:09 +0530

    Day 5

---
+++ src/array/MinimumInRotatedSorted.java
@@ -0,0 +1,44 @@
+package array;
+
+//https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+
+public class MinimumInRotatedSorted {
+	public static void main(String[] args) {
+		// int[] nums = new int[] { 4, 5, 6, 7, 0, 1, 2 };
+		// int[] nums = new int[] { 3, 4, 5, 1, 2 };
+		int[] nums = new int[] { 2, 1, 5, 4, 3 };
+		// int[] nums = new int[] { 4, 5, 6, 7, 0, 1, 2 };
+		// int[] nums = new int[] { 1 };
+		// int[] nums = new int[] { 1, 2 };
+		// int[] nums = new int[] { 1, 2, 3 };
+		// int[] nums = new int[] { 1, 2, 3, 4 };
+		// int[] nums = new int[] { 4, 3, 2, 1 };
+		// int[] nums = new int[] { 3, 2, 1 };
+		// int[] nums = new int[] { 2, 1 };
+		// int[] nums = new int[] { 2, 3, 4, 5, 1 };
+		// int[] nums = new int[] { 5, 1, 2, 3, 4 };
+		System.out.println(findMin(nums));
+	}
+
+	public static int findMin(int[] num) {
+		if (num.length == 0) {
+			return 0;
+		}
+		if (num.length == 1) {
+			return num[0];
+		}
+		int start = 0, end = num.length - 1;
+		while (start < end) {
+			int mid = start + (end - start) / 2;
+			if (mid > 0 && num[mid] < num[mid - 1]) {
+				return num[mid];
+			}
+			if (num[start] <= num[mid] && num[mid] > num[end]) {
+				start = mid + 1;
+			} else {
+				end = mid - 1;
+			}
+		}
+		return num[start];
+	}
+}
--- src/heapSort/KPairsSmallestSums.java
+++ src/heapSort/KPairsSmallestSums.java
@@ -1,20 +1,23 @@
 package heapSort;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // https://leetcode.com/problems/find-k-pairs-with-smallest-sums/
 
 public class KPairsSmallestSums {
-	public static void heapSort(int[] inputArray) {
+	public static void heapSort(int[] inputArray, List<List<Integer>> comb) {
 		for (int index = inputArray.length / 2 - 1; index >= 0; index--) {
-			heapify(index, inputArray.length, inputArray);
+			heapify(index, inputArray.length, inputArray, comb);
 		}
 
 		for (int index = inputArray.length - 1; index >= 0; index--) {
-			swap(0, index, inputArray);
-			heapify(0, index, inputArray);
+			swap(0, index, inputArray, comb);
+			heapify(0, index, inputArray, comb);
 		}
 	}
 
-	public static void heapify(int index, int end, int[] inputArray) {
+	public static void heapify(int index, int end, int[] inputArray, List<List<Integer>> comb) {
 		int largest = index;
 		int left = leftChild(index);
 		int right = rightChild(index);
@@ -25,8 +28,8 @@ public class KPairsSmallestSums {
 			largest = right;
 		}
 		if (largest != index) {
-			swap(index, largest, inputArray);
-			heapify(largest, end, inputArray);
+			swap(index, largest, inputArray, comb);
+			heapify(largest, end, inputArray, comb);
 		}
 	}
 
@@ -38,19 +41,48 @@ public class KPairsSmallestSums {
 		return ((2 * index) + 2);
 	}
 
-	public static void swap(int iIndex, int jIndex, int[] inputArray) {
+	public static void swap(int iIndex, int jIndex, int[] inputArray, List<List<Integer>> comb) {
 		int temp = inputArray[iIndex];
 		inputArray[iIndex] = inputArray[jIndex];
 		inputArray[jIndex] = temp;
+
+		List<Integer> temp1 = comb.get(iIndex);
+		comb.add(iIndex, inputArray[jIndex]);
+		inputArray[jIndex] = temp;
+
 	}
 
 	public int[] topSmalledPair(int[] nums1, int[] nums2, int k) {
+		int[] result = new int[k];
+		List<List<Integer>> comb = new ArrayList<List<Integer>>();
+		int fIndex = 0;
 		int[] finalArray = new int[nums1.length * nums2.length];
 		for (int index = 0; index < nums1.length; index++) {
 			for (int jindex = 0; jindex < nums2.length; jindex++) {
-				finalArray[2 * index + jindex] = nums1[index] + nums2[jindex];
+				List<Integer> c = new ArrayList<Integer>();
+				c.add(nums1[index]);
+				c.add(nums2[jindex]);
+				comb.add(c);
+				finalArray[fIndex++] = nums1[index] + nums2[jindex];
 			}
 		}
-		heapSort(finalArray);
+		heapSort(finalArray, comb);
+		for (int index = 0; index < k; index++) {
+			result[index] = finalArray[index];
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+
+		int[] nums1 = new int[] { 1, 7, 11 };
+		int[] nums2 = new int[] { 2, 4, 6 };
+		int k = 1;
+		KPairsSmallestSums ob = new KPairsSmallestSums();
+		int[] arr1 = ob.topSmalledPair(nums1, nums2, k);
+		for (int index = 0; index < arr1.length; index++) {
+			System.out.println(arr1[index]);
+		}
+
 	}
 }
